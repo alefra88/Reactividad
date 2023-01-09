201bda46e26fb447785916abcdf3511044234587
alefra88 <efra.rlz@gmail.com>
2023-01-09 04:52:17 -0600

    vengaa ya

--- scripts/scriptReactiveState.js
+++ scripts/scriptReactiveState.js
@@ -1,40 +1,51 @@
 const d = document;
-  
 
-  //EL STATE
-  const state = {
-    todoList:[]
-  }
+//EL STATE
+const state = {
+    todoList: [],
+};
 
-
-  //TEMPLATE UI
-  const template = () => {
-    if(state.todoList.length<1){
-      return `<p><em>Lista sin tareas por hacer</em></p>`;
+//TEMPLATE UI
+const template = () => {
+    if (state.todoList.length < 1) {
+        return `<p><em>Lista sin tareas por hacer</em></p>`;
     }
 
-    let todos = state.todoList.map(item => `<li>${item}</li>`).join("");
+    let todos = state.todoList.map((item) => `<li>${item}</li>`).join("");
 
     return todos;
-  }
+};
 
-
-  //RENDER UI
-  const render = () => {
+//RENDER UI
+const render = () => {
     console.log(state);
     const $list = d.getElementById("todo-list");
-    if(!$list) return;
+    if (!$list) return;
     $list.innerHTML = template();
-  }
+};
+// ACTUALIZAR ESTADO DE FORMA REACTIVA
+const setState = (obj) => {
+    for (let key in obj) {
+        if (state.hasOwnProperty(key)) {
+            state[key] = obj[key];
+        }
+    }
+    render();
+};
+d.addEventListener("DOMContentLoaded", render);
 
-  d.addEventListener("DOMContentLoaded",render)
+//ESTABLECIENDO VALORES POR DEFECTO AL STATE
+setState({
+    todoList: ["Tarea 1", "Tarea 2", "Tarea 3"],
+    nombre:"Alan"
+});
 
-  d.addEventListener("submit", (e) => {
+d.addEventListener("submit", (e) => {
     if (!e.target.matches("#todo-form")) return false;
 
     e.preventDefault();
     const $item = d.getElementById("todo-item");
-    if(!$item) return;
+    if (!$item) return;
     //Agrega item a la lista
     // let $li = d.createElement("li");
     // $li.textContent = $item.value;
